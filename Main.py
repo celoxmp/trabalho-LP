9fba01d6dd7d24c37ed8ef57065ce790a90e1730
celoxmp <macpcosta@gmail.com>
2019-06-23 16:51:56 -0300

    heuristicas organizadas em classes e testadas 2

--- Main.py
+++ Main.py
@@ -8,25 +8,28 @@ Created on Sat Jun 22 20:19:13 2019
 
 @author: Marcelo
 """
-print("Começando o cálculo com algoritmo genético: ")
+print("Resolução de problema caixeiro viajante com 38 cidades a serem visitadas.")
+print("Será impressa a menor distancia que cada algoritmo encontrou.")
+print()
 
+print("Começando o cálculo com algoritmo genético: ")
 ga2 = GA(EntryData)
 ga2.geneticAlgorithm(popSize=100, eliteSize=20, mutationRate=0.01, generations=100)
 print()
 
 print("Começando o cálculo com algoritmo Grasp: ")
-
 grasp = Grasp(EntryData)
 best, best_solution = grasp.calculate_GRASP(1000, 0.5)
 print(best)
 print()
 
-
 tour_guloso = grasp.nearest_neighbor_random(0,0)
 print("Algoritmo Guloso", length(tour_guloso))
 print()
+
 tour_rand_guloso = grasp.nearest_neighbor_random(0, 0.5)
 print("Algoritmo Guloso Randomizado", length(tour_rand_guloso))
 print()
+
 best, best_solution = grasp.two_opt(tour_rand_guloso)
 print("Solucao Busca Local após guloso", best_solution)
